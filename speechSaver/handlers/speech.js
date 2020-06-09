b286074435bd580fb1e473d5242ab51aa4de4214
Luke Kim <burgund32@gmail.com>
2020-06-09 15:18:55 +0900

    speechServer handler logic added

--- speechSaver/handlers/speech.js
+++ speechSaver/handlers/speech.js
@@ -1,7 +1,54 @@
+const storage = require('../storage/api')
+const dbClient = require('../models/client')
+const watsonClient = require('../watson/api')
 
-exports.saveSpeechFile = function(req,res){
+exports.saveSpeechFile = async (req, res) => {
 
-  res.statusCode = 200;
-  res.end("Hello world!")
-  
+	try {
+		const region = "korea"
+
+		var db = dbClient.getCollection(region + '-news')
+
+		const newNewsData = {
+			text: req.body.text,
+			region: region,
+			type: "info", // warning, etc
+			createdTime: Date.now()
+		}
+
+		await db.insertOne(newNewsData)
+
+		const voiceData = await watsonClient.convertToVoice(
+			req.body.text, 
+			region
+		)
+
+		const fileName = makeFileName(newNewsData)
+
+		await storage.upload(fileName, voiceData)
+
+		const newVoiceFileMetaData = {
+			key: key,
+			region: region,
+			type: "info",
+			createdTime: Date.now()
+		}
+
+		db = dbClient.getCollection(region + "-voice")
+
+		await db.insertOne(newVoiceFileMetaData)
+
+		res.statusCode = 200;
+		res.end("음성파일 생성 성공")
+
+	} catch (err) {
+
+		res.statusCode = 500;
+		console.log(err)
+		res.end(err.Message)
+	}
+}
+
+const makeFileName = (newsData) => {
+	return `${newsData.region}-${newsData.type}-${newsData.createdTime}.wav`
 }
