3621335a0172ba2d6e2fcb202c9fa80a04e4698d
박서준 <bakseo3060@gmail.com>
2020-06-11 19:15:17 +0900

    Crawler initial Setting

---
+++ crawler/README.md
@@ -0,0 +1,49 @@
+#Purpose
+Data crawling and crate raido news script
+
+### 필수 사항
+
+사전에 virtualenv crawlerCovid를 실행하여 crawlerCovid 가상환경이 생성되어 있습니다.
+source crawlerCovid/bin/activate를 실행 후 crawlerCovid 가상환경이 실행되어 있는지 확인하세요.
+
+#Download and Setting
+docker 에는 필요한 패키지랑 라이브러리들 환경 세팅 해놨습니다.
+해당 crawler를 로컬 환경에서 실행시 필요한 요소들은 아래 항목을 통해 다운로드 가능합니다.
+
+### 1. chromedirver
+
+    아래 링크를 통해 각 운영체제에 맞는 chromedriver를 설치하세요.
+    https://chromedriver.storage.googleapis.com/index.html?path=83.0.4103.39/
+
+### 2. google-cloud-language
+
+    pip install google-cloud-language
+
+### 3. google-cloud-storage
+
+    pip install google-cloud-storage
+
+### 4. bs4
+
+    pip install bs4
+
+### 5. selenium
+
+    pip install selenium
+
+#Function
+
+### 1. Set Crawler
+
+### 2. Clean Raw Data Function
+
+    a. Extract time data
+    b. Extract location data with Google Cloud Natural Language API
+    c. Create dictionary
+        key(time), value(location list)
+
+### 3. Automatic detection of new posts Function
+
+### 4. Create news scripts
+
+### 5. Send to Speech Saver
---
+++ crawler/tutorialGoogleNLP.py
@@ -0,0 +1,70 @@
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+from google.cloud import storage
+
+def sample_analyze_entities(text_content):
+    """
+    Analyzing Entities in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "ko"
+    document = {"content": text_content, "type": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = enums.EncodingType.UTF8
+
+    response = client.analyze_entities(document, encoding_type=encoding_type)
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(enums.Entity.Type(entity.type).name))
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}".format(enums.EntityMention.Type(mention.type).name)
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
+def explicit():
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        './accessKeyGoogleCloudNLP.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+if __name__ == "__main__":
+    contents = '16:18 양천구 확진자와 김포시 확진자의 자택 방문 → 19:17 천년초 바지락칼국수 → 21:56 카페 → 22:16 양천구 확진자와 김포시 확진자의 자택방문 → 22:25 자택'
+    sample_analyze_entities(contents)
