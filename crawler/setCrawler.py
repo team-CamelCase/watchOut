06af92b76bd2ede0b17a3f77de16acdb9ca95a41
박서준 <bakseo3060@gmail.com>
2020-06-11 23:40:43 +0900

    added input of css selector
    - 2n-th childs have patients' route

--- crawler/setCrawler.py
+++ crawler/setCrawler.py
@@ -13,8 +13,13 @@ driver.find_element_by_css_selector('#container > div.layout-inner.layout-sub \
 #클릭한 페이지에서 html 가져오기
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-sampleContents = soup.select('#DataTables_Table_0 > tbody > tr:nth-child(4) > td')
-print(sampleContents)
+# sampleContents = soup.select('#DataTables_Table_0 > tbody > tr:nth-child(4) > td')
+
+#확진자 이동 경로 및 시간이 있는 html 항목 tr:nth-child(2n) (n>=1)
+for i in range(1,10):
+    sampleContents = soup.select('#DataTables_Table_0 > tbody > tr:nth-child({})'.format(i))
+    print(sampleContents)
+
 
 ##필요 기능##
 # 1. Raw data to cleaned Data Function
