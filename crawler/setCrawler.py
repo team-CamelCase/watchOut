00e89398d4959271fbb99365bd59b0b88d5601e4
박서준 <bakseo3060@gmail.com>
2020-06-12 12:13:24 +0900

    Modified setCrawler

--- crawler/setCrawler.py
+++ crawler/setCrawler.py
@@ -2,24 +2,24 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class SeoulCrawler:
+    ## Set Webdriver(Used Chrome Driver)
+    driver = webdriver.Chrome('./chromedriver')
+    ## Create webdriver object from corona tab of Seoul City Hall's hompage
+    driver.get('https://www.seoul.go.kr/coronaV/coronaStatus.do')
+    ## Click confirmation path tab
+    driver.find_element_by_css_selector('#container > div.layout-inner.layout-sub \
+    > div > div.move-tab > ul > li:nth-child(2) > button').click()
+    # 클릭한 페이지에서 html 가져오기
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
     def __init__(self):
-        ## Set Webdriver(Used Chrome Driver)
-        driver = webdriver.Chrome('./chromedriver')
-        ## Create webdriver object from corona tab of Seoul City Hall's hompage
-        driver.get('https://www.seoul.go.kr/coronaV/coronaStatus.do')
-        ## Click confirmation path tab
-        driver.find_element_by_css_selector('#container > div.layout-inner.layout-sub \
-        > div > div.move-tab > ul > li:nth-child(2) > button').click()
-        #클릭한 페이지에서 html 가져오기
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-
-        #확진자 이동 경로 및 시간이 있는 html 항목 tr:nth-child(2n) (n>=1)
+        return
     def crawlRawData(self):
         contents = []
+        # 확진자 이동 경로 및 시간이 있는 html 항목 tr:nth-child(2n) (n>=1)
         for i in range(2,100,2):
             contents.append(self.soup.select('#DataTables_Table_0 > tbody > tr:nth-child({})'.format(i)))
-
+        return contents
 tempCrawler = SeoulCrawler()
 a = tempCrawler.crawlRawData()
 print(a)
