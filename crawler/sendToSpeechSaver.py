3b7736dcbb8c5a55b93099087eae289bc563ec61
박서준 <bakseo3060@gmail.com>
2020-06-13 06:16:54 +0900

    Modified sendToSpeechSaver

--- crawler/sendToSpeechSaver.py
+++ crawler/sendToSpeechSaver.py
@@ -4,7 +4,7 @@ import os
 class Transfer():
     url = ""
     def __init__(self):
-        self.url = "http://172.28.0.2:3000/api/v1/speech"
+        self.url = "http://localhost:8080/api/v1/speech"
     def sendScript(self, title, content):
         data = {"title" : title.encode('utf-8'), "content": content.encode('utf-8')}
         response = requests.post(self.url, data = data)
